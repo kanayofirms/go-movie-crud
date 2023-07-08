c630d54a551a905c0c2356f7dc83adf61699e9e4
kanayofirms <kanyeze@gmail.com>
2023-07-08 18:11:53 +0100

    Append movies to the sliceMovie

--- main.go
+++ main.go
@@ -22,11 +22,13 @@ type Director struct {
 	Lastname string `json:"lastname"` 
 }
 
-var moives []Movie
+var movies []Movie
 
 func main(){
 	r := mux.NewRouter()
 
+	movies = append(movies, Movie{ID: "1", Isbn: "433248", Title: "Airforce One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "468082", Title: "Welcome To Nigeria", Director: &Director{Firstname: "Ayodele", Lastname: "Adebanjo"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
