85e721dbbf36e6ff056024f82b91bcdb3a725ce7
kanayofirms <kanyeze@gmail.com>
2023-07-08 17:28:21 +0100

    Create struct for data type movie

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import(
 	"strconv"
 	"github.com/gorilla/mux"
 )
-/*
+
 type Movie struct {
 	ID string `json:"id"`
 	Isbn string `json:"title`
 	Title string `json:"title"`
 	Director *Director `json:"director"`
 }
-
+/*
 type Director struct {
 	Firstname  string `json:"firstname"`
 	Lastname string `json:"lastname"` 
